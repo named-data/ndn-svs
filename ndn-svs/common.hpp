2f62fb586f2ef4d17a31b1f30695eb05743a8e3e
Varun Patil <varunpatil@ucla.edu>
2020-12-31 13:22:17 +0530

    Update license in code files

--- ndn-svs/common.hpp
+++ ndn-svs/common.hpp
@@ -5,16 +5,13 @@
  * This file is part of ndn-svs, synchronization library for distributed realtime
  * applications for NDN.
  *
- * ndn-svs is free software: you can redistribute it and/or modify it under the terms
- * of the GNU General Public License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
+ * ndn-svs library is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
  *
- * ndn-svs is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- * PURPOSE.  See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * ndn-svs, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ * ndn-svs library is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
  */
 
 #ifndef NDN_SVS_COMMON_HPP
