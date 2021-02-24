29502759bb9869d7fac917e9ed27e14eaefb07cb
Varun Patil <varunpatil@ucla.edu>
2021-02-25 04:01:05 +0530

    version-vector: add static test

--- tests/unit-tests/version-vector.t.cpp
+++ tests/unit-tests/version-vector.t.cpp
@@ -64,12 +64,18 @@ BOOST_AUTO_TEST_CASE(Iterate)
 BOOST_AUTO_TEST_CASE(EncodeDecode)
 {
   ndn::Block block = v.encode();
-  BOOST_CHECK_GT(block.value_size(), 0);
-
-  // 100 bytes is too big
-  BOOST_CHECK_LT(block.value_size(), 100);
+  BOOST_CHECK_EQUAL(block.value_size(), 16);
 
   VersionVector dv(block);
+  BOOST_CHECK_EQUAL(dv.get("one"), 1);
+  BOOST_CHECK_EQUAL(dv.get("two"), 2);
+}
+
+BOOST_AUTO_TEST_CASE(DecodeStatic)
+{
+  // Hex: CA036F6E65CB0101CA0374776FCB0102
+  const char* encoded = "\xCA\x03\x6F\x6E\x65\xCB\x01\x01\xCA\x03\x74\x77\x6F\xCB\x01\x02";
+  VersionVector dv(ndn::encoding::makeBinaryBlock(ndn::tlv::Content, encoded, 16));
   BOOST_CHECK_EQUAL(dv.get("one"), 1);
   BOOST_CHECK_EQUAL(dv.get("two"), 2);
 }
