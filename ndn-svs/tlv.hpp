58c6a91ace103748bc78000e8df78e4026e7a766
Varun Patil <varunpatil@ucla.edu>
2025-01-06 03:03:48 +0000

    chore: update copyright

--- ndn-svs/tlv.hpp
+++ ndn-svs/tlv.hpp
@@ -1,3 +1,19 @@
+/* -*- Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil -*- */
+/*
+ * Copyright (c) 2012-2024 University of California, Los Angeles
+ *
+ * This file is part of ndn-svs, synchronization library for distributed realtime
+ * applications for NDN.
+ *
+ * ndn-svs library is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation, in version 2.1 of the License.
+ *
+ * ndn-svs library is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ */
+
 #ifndef NDN_SVS_TLV_HPP
 #define NDN_SVS_TLV_HPP
 
--- ndn-svs/version-vector.cpp
+++ ndn-svs/version-vector.cpp
@@ -1,18 +1,17 @@
 /* -*- Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil -*- */
 /*
- * Copyright (c) 2012-2022 University of California, Los Angeles
+ * Copyright (c) 2012-2024 University of California, Los Angeles
  *
- * This file is part of ndn-svs, synchronization library for distributed
- * realtime applications for NDN.
+ * This file is part of ndn-svs, synchronization library for distributed realtime
+ * applications for NDN.
  *
- * ndn-svs library is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation, in version 2.1 of the License.
+ * ndn-svs library is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation, in version 2.1 of the License.
  *
- * ndn-svs library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
- * for more details.
+ * ndn-svs library is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
  */
 
 #include "version-vector.hpp"
